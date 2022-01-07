9c42690834120421d71fb8be42de2f55c80b1c09
ComiiX <220-2019@ftn.edu.rs>
2022-01-07 18:08:10 +0100

    Dorada Business Layer-a

---
+++ Business_Layer/UnicomBusiness/KorisnikBusiness.cs
@@ -0,0 +1,40 @@
+﻿using MagacinData;
+using Shared.Interfaces;
+using Shared.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagacinBusiness
+{
+    public class KorisnikBusiness : IKorisnikBusiness
+    {
+        private readonly Shared.Interfaces.IKorisnikRepository korisnikRepository;
+
+        public KorisnikBusiness(Shared.Interfaces.IKorisnikRepository _korisnikRepository)
+        {
+            korisnikRepository = _korisnikRepository;
+        }
+
+        public List<Korisnik> GetAllKorisnik()
+        {
+            return this.korisnikRepository.GetAllKorisnik();
+        }
+
+        public string InsertKorisnik(Korisnik korisnik)
+        {
+            int rowsAffected = this.korisnikRepository.InsertKorisnik(korisnik);
+
+            if (rowsAffected > 0)
+            {
+                return "Uspešan unos u bazu podataka!";
+            }
+            else
+            {
+                return "Neuspešan unos, došlo je do greške!";
+            }
+        }
+    }
+}
