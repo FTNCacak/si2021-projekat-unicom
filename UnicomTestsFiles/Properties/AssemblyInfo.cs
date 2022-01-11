fafa2b0da74d8633fc62a61f387acdf4759b29e9
Veroljub Djuric <92751682+ihinsya@users.noreply.github.com>
2022-01-11 16:43:34 +0100

    Add files via upload
    Uradjene test klase za Korisnik i Zahtev. - hyakt22

---
+++ UnicomTestsFiles/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnicomTestsFiles")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnicomTestsFiles")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("83239446-cd11-49e6-83ad-12f34ce46b7f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
